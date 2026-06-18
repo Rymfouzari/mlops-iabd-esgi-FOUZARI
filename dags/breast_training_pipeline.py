0b494facfdd37b6dc867c59ce50cf1022bb25144
Rymfouzari <rymfouzari@gmail.com>
2026-06-18 23:51:34 +0200

    Simplify Airflow training pipeline

--- dags/breast_training_pipeline.py
+++ dags/breast_training_pipeline.py
@@ -16,7 +16,7 @@ DEFAULT_ENV = {
 
 with DAG(
     dag_id="breast_training_pipeline",
-    description="Validation dataset, baseline, comparaison de modeles et optimisation Optuna",
+    description="Validation dataset, baseline et optimisation Optuna",
     start_date=datetime(2026, 1, 1),
     schedule=None,
     catchup=False,
@@ -34,16 +34,10 @@ with DAG(
         env=DEFAULT_ENV,
     )
 
-    train_models = BashOperator(
-        task_id="train_models",
-        bash_command="python -m mlproject.train_models",
-        env=DEFAULT_ENV,
-    )
-
     train_optuna = BashOperator(
         task_id="train_optuna",
         bash_command="python -m mlproject.train_optuna --n-trials ${N_TRIALS:-5} --cv ${CV:-2} --no-mlflow",
         env=DEFAULT_ENV,
     )
 
-    validate_dataset >> train_baseline >> train_models >> train_optuna
+    validate_dataset >> train_baseline >> train_optuna
